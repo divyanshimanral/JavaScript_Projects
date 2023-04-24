6977bbe3f984cd8caec4254a1cea555a2e591ee6
Divyanshi <manraldivyanshi46@gmail.com>
2023-04-24 18:56:30 +0530

    quote generator added

--- quotes_generator-via-api/script.js
+++ quotes_generator-via-api/script.js
@@ -1,35 +1,29 @@
 let quote = document.getElementById("quote");
 let author = document.getElementById("author");
 let btn = document.getElementById("btn");
-// let btn2 = document.getElementById("btn2");
+let saveBtn = document.getElementById("save-btn");
 
-const favoriteQuotes = [];
-const url = "https://api.quotable.io/random";
+const favoriteQuotes = JSON.parse(localStorage.getItem("favoriteQuotes")) || [];
+const url = "https://api.quotable.io/random"; 
 
 let getQuote = () => {
   fetch(url)
     .then((data) => data.json())
     .then((item) => {
-      console.log(item.content);
-      console.log(item.author);
       quote.innerText = item.content;
       author.innerText = item.author;
     });
-};
-
+  };
+  
 window.addEventListener("load", getQuote);
 btn.addEventListener("click", getQuote);
-// btn2.addEventListener("click", () => {
-//   favoriteQuotes.push();
-//   console.log("Quote added to favorites");
-// });
 
-// const favoritesList = document.createElement("ul");
-// for (let i = 0; i < favoriteQuotes.length; i++) {
-//   const listItem = document.createElement("li");
-//   listItem.textContent = favoriteQuotes[i];
-//   favoritesList.appendChild(listItem);
-// }
-// document.body.appendChild(favoritesList);
+saveBtn.addEventListener("click", () => {
+  const quoteText = quote.innerText;
+  const quoteAuthor = author.innerText;
+  const quoteObj = {quote: quoteText, author: quoteAuthor};
+  favoriteQuotes.push(quoteObj);
+  localStorage.setItem("favoriteQuotes", JSON.stringify(favoriteQuotes));
+})
 
 
